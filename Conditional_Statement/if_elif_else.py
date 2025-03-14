81bea1391ea13f6d25c13eddc8e451615205ee4c
Code-with-anvesha <sharmaanvesha905@gmail.com>
2025-03-14 21:56:18 +0530

    Added if_elif_else.py

---
+++ Conditional_Statement/if_elif_else.py
@@ -0,0 +1,19 @@
+score = 180
+if score >= 100:
+    print("Please verify your grade again")
+    
+      exit()
+
+        if score >= 90:
+            grade = "A"
+            elif score >= 80:
+                grade = "B"
+                elif score >= 70:
+                    grade = "c"
+                    elif score >= 60:
+                        grade = "D"
+                        else:
+                            grade = "F"
+
+                            print("Grade:", grade)
+
