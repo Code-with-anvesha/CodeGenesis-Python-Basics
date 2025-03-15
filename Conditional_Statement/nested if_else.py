4d6c9283892dc118dbcf026927041af0e80cb464
Anvesh Sharma <163885872+Code-with-anvesha@users.noreply.github.com>
2025-03-15 22:58:33 +0530

    Add files via upload

---
+++ Conditional_Statement/nested if_else.py
@@ -0,0 +1,10 @@
+fruit = "Apple"
+color = "Red"
+
+if fruit == "Apple":
+    if color == "Yellow":
+        print("Unkill")
+    elif color == "Red":
+        print("Kill")
+    elif color == "Green":
+        print(OverKill) 
