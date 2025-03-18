f9c39dbefd344f3be5b33f5bd22a24ff3e89fa7a
Code-with-anvesha <sharmaanvesha905@gmail.com>
2025-03-18 23:13:31 +0530

    Added pet_food_checker.py file

---
+++ Conditional_Statement/pet_food_checker.py
@@ -0,0 +1,21 @@
+pet_type = input("Enter your pet name : ").strip().lower()
+age = int(input("Enter your pet's age (in years): "))
+
+# Pet food recommendation logic
+if pet_type == "dog":
+    if age < 2:
+        food = "Puppy Food"
+    else:
+        food = "Adult Dog Food"
+elif pet_type == "cat":
+    if age <= 2:
+        food = "Kitten Food"
+    elif age <= 5:
+        food = "Adult Cat Food"
+    else:
+        food = "Senior Cat Food"
+else:
+    food = "Unknown pet type! Please enter Dog or Cat."
+
+# Final output
+print("Recommended Food:", food)
