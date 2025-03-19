a03a54d36ac98de3de2f322865cb8afa8c1d0d6e
Code-with-anvesha <sharmaanvesha905@gmail.com>
2025-03-19 23:37:38 +0530

    Added tuple_operations.py file

---
+++ _Data_Structures/tuple_operations.py
@@ -0,0 +1,11 @@
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+
+combined = tuple1 + tuple2
+print("Combined Tuple:", combined)
+
+repeated = tuple1 * 2
+print("Repeated Tuple:", repeated)
+
+print("Is 2 in tuple1?", 2 in tuple1)
+
