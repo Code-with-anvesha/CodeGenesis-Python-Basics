bc78aaaefa2f63c984a2084240b95dbe0b2fbaf7
Code-with-anvesha <sharmaanvesha905@gmail.com>
2025-03-19 23:30:28 +0530

    Added tuple_unpacking.py file

---
+++ _Data_Structures/tuple_unpacking.py
@@ -0,0 +1,7 @@
+student = ("Anvesha", 20, "BCA", "JNP")
+
+name, age, course, address = student
+print("Student Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("Address:", address)
