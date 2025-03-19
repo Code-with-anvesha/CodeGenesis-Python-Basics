32acd78234f06af2d8ff7d28adf91c007371777c
Code-with-anvesha <sharmaanvesha905@gmail.com>
2025-03-19 23:16:26 +0530

    Modified with Updated  dict_operations.py file

--- _Data_Structures/dict_operations.py
+++ _Data_Structures/dict_operations.py
@@ -2,14 +2,11 @@
 person = {"name": "Anvesha", "age": 20}
 
 # Add key-value pair
-
 person["city"] = "Jaunpur"
 
 # Access value using key
-print(person["name"])
-# output Anvesha
+print(person["name"])  # Output: Anvesha
 
 # Loop through dictionary
-  for key, value in
-        person.items(): # type: ignore
-       print(f"{key}: {value}")
+for key, value in person.items(): 
+    print(f"{key}: {value}")  
